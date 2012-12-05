900a442d05363a2c505ed82db01e49e84ad3c65d
Fernando Jorge Mota <f.j.mota13@gmail.com>
2012-12-05 00:49:52 -0200

    Added package to describe listeners to receive events from the JHandControl class

---
+++ src/jhandcontrol/events/FrameListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jhandcontrol.events;
+
+import jhandcontrol.data.JFrameHand;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface FrameListener {
+    public void frameEvent(JFrameHand frame);
+}
