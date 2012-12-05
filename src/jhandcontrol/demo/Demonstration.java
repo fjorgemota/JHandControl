78407d99c0cb4da94c6fa3ba58c242b4e1c485a2
Fernando Jorge Mota <f.j.mota13@gmail.com>
2012-12-05 00:50:09 -0200

    Updated demo

--- src/jhandcontrol/demo/Demonstration.java
+++ src/jhandcontrol/demo/Demonstration.java
@@ -15,7 +15,35 @@ import javax.swing.JFrame;
 import jhandcontrol.JHandControl;
 import jhandcontrol.calibrator.utils.Mouse;
 import jhandcontrol.data.HandStatus;
+import jhandcontrol.data.JFrameHand;
 import jhandcontrol.data.JHandDetection;
+import jhandcontrol.events.FrameListener;
+
+class HandListener implements FrameListener {
+
+    private JHandDetection hand;
+
+    public HandListener() {
+        this.hand = null;
+    }
+
+    @Override
+    public void frameEvent(JFrameHand frame) {
+        ArrayList<JHandDetection> hands = frame.getHands();
+        if(hands.isEmpty()){
+            return;
+        }
+        for (JHandDetection tempHand : hands) {
+            if (tempHand.getStatus() != HandStatus.UNRECOGNIZED) {
+                this.hand = tempHand;
+                break;
+            }
+        }
+    }
+    public JHandDetection getHand(){
+        return this.hand;
+    }
+}
 
 /**
  *
@@ -27,8 +55,10 @@ public class Demonstration {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        HandListener listener = new HandListener();
         JHandControl.setDefaultCamera(0);
         JHandControl lib = JHandControl.getInstance();
+        lib.addFrameListener(listener);
         lib.start();
         Mouse mouse = new Mouse();
         JFrame demoWindow = new JFrame("Demonstracao");
@@ -43,16 +73,17 @@ public class Demonstration {
         JHandDetection hand = null;
         rect.setBounds(10, 50, 100, 100);
         while (true) {
+            try {
+                Thread.sleep(1000 / 50);
+            } catch (Exception ex) {
+            }
             Graphics g = bf.getDrawGraphics();
             g.clearRect(0, 0, 800, 600);
-            List<JHandDetection> hands = lib.getHands();
+            hand = listener.getHand();
             g.setColor(Color.BLACK);
             g.drawRect(10, 50, 100, 100);
             g.drawString("Calibrar", 40, 100);
-            if (!hands.isEmpty()) {
-                hand = hands.get(0);
-            }
-            if(hand != null){
+            if (hand != null) {
                 if (hand.getStatus() == HandStatus.OPEN) {
                     g.setColor(Color.RED);
                 } else {
